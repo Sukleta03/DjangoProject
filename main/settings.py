7fcad159af982d5d1bf7996b37275567be559941
Sukleta Dima <dima.sukleta03@gmail.com>
2023-08-18 22:01:59 +0300

    Task-12, commit 1.3.15:

--- main/settings.py
+++ main/settings.py
@@ -127,14 +127,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = '/static/'
-MEDIA_URL = '/media/'
+# STATIC_URL = '/static/'
+# MEDIA_URL = '/media/'
+#
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+#
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static'),
+# ]
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+STATIC_URL = '/home/ubuntu/static/'
+MEDIA_URL = '/home/ubuntu/media/'
 
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
