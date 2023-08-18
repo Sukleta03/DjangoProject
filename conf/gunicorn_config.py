a1c61142bfa21fff39690e2fc2f55137dd187508
Sukleta Dima <dima.sukleta03@gmail.com>
2023-08-18 21:54:10 +0300

    Task-12, commit 1.3.14:

--- conf/gunicorn_config.py
+++ conf/gunicorn_config.py
@@ -1,6 +1,6 @@
 
-command = '/home/ubnutu/task-12/env/bin/gunicorn'
-pythonpath = '/home/ubnutu/task-12'
+command = '/home/ubuntu/task-12/env/bin/gunicorn'
+pythonpath = '/home/ubuntu/task-12'
 bind = '13.51.150.26:8000'
 workers = 3
 
